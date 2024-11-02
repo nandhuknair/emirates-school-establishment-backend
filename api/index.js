edb6b81ccbdbf9a7baa4bbf3747126107dd12a6b
nandhuknair <nandhuknair27@gmail.com>
2024-11-02 15:47:51 +0530

    add backend cors

--- api/index.js
+++ api/index.js
@@ -11,7 +11,13 @@ app.use(cors({
   methods: ['GET', 'POST'],
 }));
 
-// app.use(express.json())
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.use(express.json())
 // app.use(cookiesParser())
 
 const PORT = process.env.PORT || 8080
